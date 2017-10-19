db3e14b633f184da0b214ebc83235dc3d92f8e4f
ArunoxPL <greg.malek@gmail.com>
2017-10-19 16:31:59 +0200

    xml .net framework

---
+++ Capgemini/Customer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+
+namespace Capgemini
+{
+    public class Customer
+    {
+        private List<Customer> _customerList = new List<Customer>();
+
+        protected string Name { get; set; }
+        protected string Surname { get; set; }
+        protected int TelephoneNumber { get; set; }
+        protected string Address { get; set; }
+
+        public Customer(string name, string surname, int telephoneNumber, string address)
+        {
+            Name = name;
+            Surname = surname;
+            TelephoneNumber = telephoneNumber;
+            Address = address;
+        }
+
+        public void CreatingXml()
+        {
+            XDocument xml = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XComment("Customer list"),
+                new XElement("customers",
+                    from customer in _customerList
+                    orderby customer.Name
+                    select new XElement("customer",
+                        new XAttribute("name", customer.Name),
+                        new XElement("surname", customer.Surname),
+                        new XElement("telephone", customer.TelephoneNumber),
+                        new XElement("address", customer.Address)
+                    )
+                )
+            );
+            Console.WriteLine(xml);
+            xml.Save(@"customer.xml");
+        }
+
+
+        public void AddingCustomer()
+        {
+            Console.WriteLine("Write the Name of customer: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Write the Surname of customer: ");
+            Surname = Console.ReadLine();
+            Console.WriteLine("Write the Tel. number of customer: ");
+            TelephoneNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write the Address of customer: ");
+            Address = Console.ReadLine();
+
+            _customerList.Add(new Customer(Name, Surname, TelephoneNumber, Address));
+
+        }
+
+    }
+
+}
---
+++ Capgemini/Worker.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Capgemini;
+
+namespace Capgemini
+{
+    public class Worker
+    {
+        Customer customer = new Customer("", "", 0, "");
+        bool exit = false;
+
+        public Worker()
+        {
+        }
+
+        public void Menu()
+        {
+            customer.CreatingXml();
+            do
+            {
+                int? result = null;
+                Console.WriteLine("Welcome in Menu, what do you want to do? \n Add new Customer press 1 \n Update Customer press 2 \n Delete Customer press 3 \n Exit press 4");
+
+                try
+                {
+                    result = Convert.ToInt16(Console.ReadLine());
+
+                    switch (result)
+                    {
+                        case 1:
+                            {
+                                customer.AddingCustomer();
+                                Console.WriteLine(" Add menu works");
+                            }
+                            break;
+                        case 2:
+                            {
+                                Console.WriteLine(" Delete menu works");
+
+                            }
+                            break;
+                        case 3:
+                            Console.WriteLine(" Update menu works");
+                            break;
+                        case 4:
+                            {
+                                exit = true;
+                                Console.WriteLine(" Exit menu works");
+                            }
+                            break;
+                        default:
+                            Console.WriteLine(" Wrong Parameter Works");
+                            break;
+                    }
+                }
+                catch (FormatException fEx)
+                {
+                    Console.WriteLine(fEx.Message);
+                }
+                catch (Exception Ex)
+                {
+                    Console.WriteLine("Something went wrong");
+                }
+
+            } while (exit != true);
+        }
+    }
+}
